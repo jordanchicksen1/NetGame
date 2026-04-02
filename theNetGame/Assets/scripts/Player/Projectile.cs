22d590144e4b2ce7722a48b886c5cfe70e87eaef
jordanchicksen1 <2333640@students.wits.ac.za>
2026-04-02 14:35:26 +0200

    progress on spells

--- theNetGame/Assets/scripts/Player/Projectile.cs
+++ theNetGame/Assets/scripts/Player/Projectile.cs
@@ -3,37 +3,48 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
-    [SerializeField] float lifetime = 3f;
+    [SerializeField] float lifetime = 5f;
+    [SerializeField] int maxBounces = 3;
 
+    Rigidbody2D rb;
+    int bounceCount;
     float direction;
 
     public void Initialize(float dir)
     {
         direction = dir;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        // Initial forward velocity
+        rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+
         Destroy(gameObject, lifetime);
     }
 
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+        // Bounce off ground
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            PlayerController2D player = collision.GetComponent<PlayerController2D>();
+            bounceCount++;
+
+            if (bounceCount >= maxBounces)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Hit player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController2D player = collision.gameObject.GetComponent<PlayerController2D>();
 
             if (player != null)
             {
                 player.LoseSpell();
             }
 
-            Destroy(gameObject);
-        }
-
-        if (collision.CompareTag("Ground"))
-        {
             Destroy(gameObject);
         }
     }
