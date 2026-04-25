df9599ca46397f83553299114d0a1e8b6e77db39
RamzCas <2749414@students.wits.ac.za>
2026-04-25 13:54:15 +0200

    anims

---
+++ theNetGame/Assets/scripts/MusicManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Sources")]
+    public AudioSource BackGroundMusic;
+    public AudioSource SFX;
+
+    [Header("CLips")]
+    public AudioClip Coin;
+    public AudioClip Hit;
+    public AudioClip Jump;
+    public AudioClip PowerUp;
+    public AudioClip TenthCoin;
+}
