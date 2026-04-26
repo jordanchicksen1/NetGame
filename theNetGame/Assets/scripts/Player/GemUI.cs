5ddfd3019a6aaa958cee4d8fa0dca16df911dd71
jordanchicksen1 <2333640@students.wits.ac.za>
2026-04-26 17:29:47 +0200

    gem counter working

---
+++ theNetGame/Assets/scripts/Player/GemUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class GemUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI gemText;
+
+    PlayerController2D player;
+
+    void Start()
+    {
+        StartCoroutine(FindPlayerRoutine());
+    }
+
+    IEnumerator FindPlayerRoutine()
+    {
+        while (player == null)
+        {
+            var players = FindObjectsByType<PlayerController2D>(FindObjectsSortMode.None);
+
+            foreach (var p in players)
+            {
+                if (p.IsOwner)
+                {
+                    player = p;
+                    break;
+                }
+            }
+
+            yield return null; // wait 1 frame and try again
+        }
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        gemText.text = $"Gems: {player.GetGemCount()} / 10";
+    }
+}
