c9e390285319427de6bdc0303e07c1e92289e755
jordanchicksen1 <2333640@students.wits.ac.za>
2026-04-27 23:43:24 +0200

    final push i think

--- theNetGame/Assets/scripts/Player/LocalUIButtons.cs
+++ theNetGame/Assets/scripts/Player/LocalUIButtons.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 public class LocalUIButtons : MonoBehaviour
 {
+    public GameObject controlsPage;
+    public GameObject howToStartAGamePage;
+    public GameObject howToPlayPanel;
+    public GameObject instruction1;
+    public GameObject instruction2;
+    public GameObject instruction3;
     public void StartButton()
     {
         StartCoroutine(StartTheGame());
@@ -14,5 +20,69 @@ public class LocalUIButtons : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("MainMenu");
 
+    }
+
+    public void ControlsPageButton()
+    {
+        controlsPage.SetActive(true);
+    }
+
+    public void ExitControlsPageButton()
+    {
+        controlsPage.SetActive(false);
+    }
+
+    public void HowToStartPageButton()
+    {
+        howToStartAGamePage.SetActive(true);
+    }
+
+    public void ExitHowToStartPageButton()
+    {
+        howToStartAGamePage.SetActive(false);
+    }
+
+    public void HowToPlayButton()
+    {
+        howToPlayPanel.SetActive(true);
+        instruction1.SetActive(true);
+        instruction2.SetActive(false);
+        instruction3.SetActive(false);
+    }
+
+    public void nextInstruction1()
+    {
+        instruction1.SetActive(false);
+        instruction2.SetActive(true);
+        instruction3.SetActive(false);
+    }
+
+    public void nextInstruction2()
+    {
+        instruction1.SetActive(false);
+        instruction2.SetActive(false);
+        instruction3.SetActive(true);
+    }
+
+    public void previousInstruction2()
+    {
+        instruction1.SetActive(true);
+        instruction2.SetActive(false);
+        instruction3.SetActive(false);
+    }
+
+    public void previousInstruction3()
+    {
+        instruction1.SetActive(false);
+        instruction2.SetActive(true);
+        instruction3.SetActive(false);
+    }
+
+    public void ExitHowToPlayButton()
+    {
+        howToPlayPanel.SetActive(false);
+        instruction1.SetActive(false);
+        instruction2.SetActive(false);
+        instruction3.SetActive(false);
     }
 }
