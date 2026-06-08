3c0843c8d6495ad2a7633c4ce4f361c11effb112
jordanchicksen1 <2333640@students.wits.ac.za>
2026-06-08 13:45:35 +0200

    vfx and sfx for hiding spots

--- theNetGame/Assets/scripts/Player/HidingSpot.cs
+++ theNetGame/Assets/scripts/Player/HidingSpot.cs
@@ -6,6 +6,17 @@ public class HidingSpot : NetworkBehaviour
     public PlayerController2D Occupant { get; private set; }
 
     public bool IsOccupied => Occupant != null;
+    
+    [SerializeField] AudioClip hideSound;
+    [SerializeField] AudioClip exitSound;
+    [SerializeField] AudioClip breakSound;
+    AudioSource audioSource;
+    [SerializeField] GameObject breakParticlesPrefab;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public bool TryHide(PlayerController2D player)
     {
@@ -15,12 +26,17 @@ public class HidingSpot : NetworkBehaviour
             return false;
 
         Occupant = player;
+
+        PlayHideSoundClientRpc();
+
         return true;
     }
 
     public void ExitHide()
     {
         Occupant = null;
+
+        PlayExitSoundClientRpc();
     }
 
 
@@ -35,6 +51,8 @@ public class HidingSpot : NetworkBehaviour
         if (Occupant == null)
             return;
 
+        SpawnBreakEffectsClientRpc();
+
         Occupant.ForceExitHide();
 
         GetComponent<NetworkObject>().Despawn();
@@ -43,5 +61,53 @@ public class HidingSpot : NetworkBehaviour
     public void SetOccupant(PlayerController2D player)
     {
         Occupant = player;
+    }
+
+    [ClientRpc]
+    void PlayHideSoundClientRpc()
+    {
+        Debug.Log($"Hide Sound = {hideSound}");
+
+        if (hideSound != null)
+        {
+            audioSource.PlayOneShot(hideSound);
+        }
+    }
+
+    [ClientRpc]
+    void PlayExitSoundClientRpc()
+    {
+        if (exitSound != null)
+        {
+            audioSource.PlayOneShot(exitSound);
+        }
+    }
+
+    [ClientRpc]
+    void SpawnBreakEffectsClientRpc()
+    {
+        if (breakParticlesPrefab != null)
+        {
+            Instantiate(
+                breakParticlesPrefab,
+                transform.position,
+                Quaternion.identity
+            );
+        }
+
+        if (breakSound != null)
+        {
+            GameObject tempAudio = new GameObject("BreakSound");
+
+            AudioSource source =
+                tempAudio.AddComponent<AudioSource>();
+
+            source.PlayOneShot(breakSound);
+
+            Destroy(
+                tempAudio,
+                breakSound.length
+            );
+        }
     }
 }
